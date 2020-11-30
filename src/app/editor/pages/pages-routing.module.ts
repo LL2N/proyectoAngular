69ffa87a26790dbd0a4280c3437070ef4862c3cd
luvellana <luciananunezl99@gmail.com>
2020-11-30 19:39:35 -0400

    actualizacion

--- src/app/editor/pages/pages-routing.module.ts
+++ src/app/editor/pages/pages-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import { NYCComponent } from './nyc/nyc.component';
 import {PagesComponent} from './pages.component';
 
 const routes: Routes = [
@@ -11,7 +10,7 @@ const routes: Routes = [
       {path: '', redirectTo: 'nyc', pathMatch: 'full'},
       {path: 'nyc', loadChildren: () => import('./nyc/nyc.module').then(m => m.NycModule)},
       {path: 'rutas', loadChildren: () => import('./rutas/rutas.module').then(m => m.RutasModule)},
-      {path: 'sobrelpb', loadChildren: () => import('./sobre-lpb/sobre-lpb.module').then(m => m.SobreLPBModule)}
+      {path: 'sobrelpb', loadChildren: () => import('./sobre-lpbhistoria/sobre-lpbhistoria.module').then(m => m.SobreLPBModule)}
     ]
   }
 ];
