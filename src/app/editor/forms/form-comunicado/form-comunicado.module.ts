59c82d2ea25615b6ce1ec6000fa7410058a21a49
luvellana <luciananunezl99@gmail.com>
2020-12-05 02:31:06 -0400

    saving

--- src/app/editor/forms/form-comunicado/form-comunicado.module.ts
+++ src/app/editor/forms/form-comunicado/form-comunicado.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComunicadoComponent } from './form-comunicado.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: '', component: FormComunicadoComponent}
@@ -9,7 +10,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule.forChild(routes)
+    CommonModule, RouterModule.forChild(routes),
+    ReactiveFormsModule
   ],
   exports: [RouterModule],
   declarations: [FormComunicadoComponent]
