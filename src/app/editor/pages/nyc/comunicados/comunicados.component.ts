5d220fe86116a4f0887592b4d63da7f69221c2dc
luvellana <luciananunezl99@gmail.com>
2020-12-07 02:00:56 -0400

    just Saving

--- src/app/editor/pages/nyc/comunicados/comunicados.component.ts
+++ src/app/editor/pages/nyc/comunicados/comunicados.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ComunicadosService } from '../../../../shared/services/comunicados.service';
 
 @Component({
@@ -9,22 +10,39 @@ import { ComunicadosService } from '../../../../shared/services/comunicados.serv
 })
 export class ComunicadosComponent implements OnInit {
 
+  
+  comunicadoDeleteSubs: Subscription;
   comunicados = [];
+  comunicadosGetSubs: Subscription;
 
   constructor(private ComunicadosService: ComunicadosService, private router: Router) { 
     
   }
 
   ngOnInit() {
+
+    this.loadComunicados();
     this.ComunicadosService.getComunicados().subscribe( res => {
       Object.entries(res).map(comunicado => this.comunicados.push(comunicado[1]))
-    }
+    });
+  }
 
-    )
+  loadComunicados(): void {
+    this.comunicados = [];
+
+    this.comunicadosGetSubs = this.ComunicadosService.getComunicados().subscribe( res => {
+      Object.entries(res).map(comunicado => this.comunicados.push(comunicado[1]))
+    });
   }
 
   openForm(){
     this.router.navigate(['editor/forms/formcomunicado']);
+  }
+
+  onDelete(id: any): void {
+    this.comunicadoDeleteSubs = this.ComunicadosService.deleteComunicado(id).subscribe(() => this.loadComunicados());
+
+    console.log('Comunicado: ', this.ComunicadosService.getComunicadoById(id));
   }
 
 }
