e9176d62e6d14a2cf18acae7af500ac2aa6ee130
luvellana <luciananunezl99@gmail.com>
2020-12-17 03:05:53 -0400

    all deletes

--- src/app/editor/pages/rutas/horarios/horarios.component.ts
+++ src/app/editor/pages/rutas/horarios/horarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { HorariosService } from '../../../../shared/services/horarios.service';
 
 @Component({
@@ -10,16 +11,41 @@ import { HorariosService } from '../../../../shared/services/horarios.service';
 export class HorariosComponent implements OnInit {
   horarios = [];
 
+    
+  horarioSubs: Subscription;
+  horarioGetSubs: Subscription;
+  horarioDeleteSubs: Subscription;
+  horarioUpdateSubs: Subscription;
+
   constructor(private HorariosService: HorariosService, private router: Router) { 
     
   }
 
   ngOnInit() {
-    this.HorariosService.getHorarios().subscribe( res => {
-      Object.entries(res).map(horario => this.horarios.push(horario[1]))
+    this.loadProduct();
+    
+  }
+
+  loadProduct(): void {
+    this.horarios = [];
+    this.horarioGetSubs = this.HorariosService.getHorarios().subscribe( res => {
+      Object.entries(res).map((p: any) => this.horarios.push({id: p[0], ...p[1]}));
     }
 
     )
+  }
+
+  onDelete(id: any): void {
+    console.log('RESPONSE: ', id);
+    this.horarioDeleteSubs = this.HorariosService.deleteHorario(id).subscribe(
+      res => {
+        console.log('RESPONSE: ', res);
+        this.loadProduct();
+      },
+      err => {
+        console.log('ERROR: ');
+      }
+    );
   }
     openForm(){
     this.router.navigate(['editor/forms/formhorario']);
--- src/app/editor/pages/rutas/lista/lista.component.ts
+++ src/app/editor/pages/rutas/lista/lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { RutasService } from '../../../../shared/services/rutas.service';
 
 @Component({
@@ -9,18 +10,42 @@ import { RutasService } from '../../../../shared/services/rutas.service';
 })
 export class ListaComponent implements OnInit {
 
-  rutas = [];
+  rutas = [];    
+  rutaSubs: Subscription;
+  rutaGetSubs: Subscription;
+  rutaDeleteSubs: Subscription;
+  rutaUpdateSubs: Subscription;
 
   constructor(private RutasService: RutasService, private router: Router) { 
     
   }
 
   ngOnInit() {
-    this.RutasService.getRutas().subscribe( res => {
-      Object.entries(res).map(ruta => this.rutas.push(ruta[1]))
+
+    this.loadProduct();
+    
+  }
+
+  loadProduct(): void {
+    this.rutas = [];
+    this.rutaGetSubs = this.RutasService.getRutas().subscribe( res => {
+      Object.entries(res).map((p: any) => this.rutas.push({id: p[0], ...p[1]}));
     }
 
     )
+  }
+
+  onDelete(id: any): void {
+    console.log('RESPONSE: ', id);
+    this.rutaDeleteSubs = this.RutasService.deleteRuta(id).subscribe(
+      res => {
+        console.log('RESPONSE: ', res);
+        this.loadProduct();
+      },
+      err => {
+        console.log('ERROR: ');
+      }
+    );
   }
 
   openForm(){
