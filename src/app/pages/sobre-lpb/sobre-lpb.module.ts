63809d04e0f22858b4bb3276248ac47201233d69
luvellana <luciananunezl99@gmail.com>
2020-12-18 01:19:17 -0400

    arreglo html

--- src/app/pages/sobre-lpb/sobre-lpb.module.ts
+++ src/app/pages/sobre-lpb/sobre-lpb.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuienessomosComponent } from './quienessomos/quienessomos.component';
-import { DYOComponent, DyoComponent } from './dyo/dyo.component';
-import { HistoriaComponent } from './historia/historia.component';
+
 import { Routes, RouterModule } from "@angular/router";
 import { SobreLPBComponent } from './sobre-lpb.component';
 
